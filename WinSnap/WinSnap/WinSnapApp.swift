19c746c8319a2bf3292af5e6db150b3076e0dcb3
swaggyAbbyLeeMiller <aneff701@gmail.com>
2026-01-31 15:45:42 -0500

    install swift deps

---
+++ WinSnap/WinSnap/WinSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinSnapApp.swift
+//  WinSnap
+//
+//  Created by alexa neff on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct WinSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
